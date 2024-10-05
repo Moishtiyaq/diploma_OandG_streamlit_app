1b8a90be80f32ac5bf8050c95096f96a160edbc4
Moishtiyaq <mo.ishtiyaq786u@gmail.com>
2024-10-05 09:29:25 +0530

    ECD Calculator

--- ecd_calc.py
+++ ecd_calc.py
@@ -3,8 +3,10 @@ import numpy as np
 import pandas as pd
 
 st.subheader("ECD Calculator")
-mw = st.sidebar.number_input("enter the mud weight value (ppg): ")
-apl = st.sidebar.number_input("enter the annular pressure loss value(psi): ")
-tvd = st.sidebar.number_input("enter the true vertical depth (ft): ")
+mw = st.sidebar.number_input("enter the mud weight value (ppg): ", value=9)
+apl = st.sidebar.number_input("enter the annular pressure loss value(psi): ", value=350)
+tvd = st.sidebar.number_input("enter the true vertical depth (ft): ", value=6000)
 ecd = ((apl/(0.052*tvd))+mw)
-st.write(f"the equivalent density on this tvd {tvd}ft is {ecd}")
+b = st.button("Run ECD Calculator")
+if b:
+   st.write(f"the equivalent circulating density on this tvd {tvd}ft is {ecd}")
