2e1e9010c2f825810d976590f81fd2407b41433d
Moishtiyaq <mo.ishtiyaq786u@gmail.com>
2024-10-05 10:07:27 +0530

    ECD calculator

--- ecd_calc.py
+++ ecd_calc.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 
 st.subheader("ECD Calculator")
+st.sidebar.title("User Inputs")
 mw = st.sidebar.number_input("enter the mud weight value (ppg): ", value=9)
 apl = st.sidebar.number_input("enter the annular pressure loss value(psi): ", value=350)
 tvd = st.sidebar.number_input("enter the true vertical depth (ft): ", value=6000)
